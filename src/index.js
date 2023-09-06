c98d7e6d7597413b60e64de3a7bb2d8fa957ba43
rodrigodeleon <20547168+rodrigodeleon@users.noreply.github.com>
2023-09-06 21:37:05 +0200

    rearrange code structure

--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
-import { format } from "date-fns";
-import { populateData } from "./loadData";
+import { loadAllProjects } from "./loadData";
 
-populateData();
-const projects = JSON.parse(localStorage.getItem("projects"));
 const content = document.querySelector('#content');
-const projectList = document.createElement('ul');
-projects.forEach(project => {
-    const todolist = document.createElement('ul');
-    const projName = document.createElement('li');
-    projName.textContent = project.name;
-    projName.classList.add('projName');
-    projectList.appendChild(projName);
-    project["toDoList"].forEach(element => {
-        const todo = document.createElement('li');
-        todo.textContent = element.title + ' Date: '+ element.dueDate; 
-        todolist.append(todo);
-    });
-    projectList.appeChild(todolist);
-});
-content.appendChild(projectList);
 
+content.append(loadAllProjects());
 
