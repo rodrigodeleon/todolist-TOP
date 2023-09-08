d83274649d0e8a8fad3d9a5a8f2af799e16f0e25
rodrigodeleon <20547168+rodrigodeleon@users.noreply.github.com>
2023-09-08 15:57:14 +0200

    add functionality to selector and fix appereance

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import { loadMenu, populateData } from "./loadData";
 import "./style.css";
-import * as projects from "./project";
 
 const content = document.querySelector("#content");
 const actions = document.querySelector("#actions");
-const allprojects = JSON.parse(localStorage.getItem("projects"));
+
 populateData();
 loadMenu();
