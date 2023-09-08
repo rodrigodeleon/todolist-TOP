0fda633485ab19b329e522cf5bd5cf5cd55dc781
rodrigodeleon <20547168+rodrigodeleon@users.noreply.github.com>
2023-09-08 19:11:35 +0200

    update selector functionality

--- src/project.js
+++ src/project.js
@@ -4,34 +4,26 @@ export default class Project {
     this.toDoList = [];
   }
 
-  // getToDoList() {
-  //   return this.toDoList;
-  // }
-
   addToDo(toDo) {
     this.toDoList.push(toDo);
   }
 }
-export function getProjectByName(projName)
-{
+export function getProjectByName(projName) {
   const projects = JSON.parse(localStorage.getItem("projects"));
-  let res = [];      
+  let res = [];
   projects.forEach((element) => {
     if (element.name == projName) {
       res.push(element);
     }
   });
   return res;
-  
 }
-
-
 
 export function deleteToDo(todotodelete, projectName) {
   const projects = JSON.parse(localStorage.getItem("projects"));
   projects.forEach((element) => {
     if (element.name == projectName) {
-      element.toDoList.forEach((todo , index) => {
+      element.toDoList.forEach((todo, index) => {
         if (todo.title == todotodelete) {
           console.table(element.toDoList);
           element.toDoList.splice(index, 1);
@@ -41,4 +33,14 @@ export function deleteToDo(todotodelete, projectName) {
     }
   });
   localStorage.setItem("projects", JSON.stringify(projects));
+}
+export function newProject(projectName) {
+
+  const projects = JSON.parse(localStorage.getItem("projects"));
+  const myproj = new Project(projectName);
+  projects.push(myproj);
+  localStorage.setItem("projects", JSON.stringify(projects));
+  
+  
+
 }
